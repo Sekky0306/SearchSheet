df8e104f917199facf129c6316918879f8fd367b
Sekky <step1336@gmail.com>
2015-05-19 06:55:30 +0900

    back commit

---
+++ SearchSheet/SearchViewController.h
@@ -0,0 +1,14 @@
+//
+//  SearchViewController.h
+//  SearchSheet
+//
+//  Created by 関戸優紀 on 2015/05/18.
+//  Copyright (c) 2015年 関戸優紀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+-(IBAction)back;
+
+@end
---
+++ SearchSheet/SearchViewController.m
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.m
+//  SearchSheet
+//
+//  Created by 関戸優紀 on 2015/05/18.
+//  Copyright (c) 2015年 関戸優紀. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@implementation SearchViewController
+
+
+-(IBAction)back{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
