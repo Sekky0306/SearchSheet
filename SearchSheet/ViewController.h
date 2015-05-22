99d9a70f3cc2f7ddb1a5aab36baa424f11829300
Sekky <step1336@gmail.com>
2015-05-22 22:14:57 +0900

    picker commit

--- SearchSheet/ViewController.h
+++ SearchSheet/ViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+<UIPickerViewDelegate, UIPickerViewDataSource,UITextFieldDelegate>
 
 
 @end
--- SearchSheet/ViewController.m
+++ SearchSheet/ViewController.m
@@ -12,16 +12,80 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    IBOutlet UIPickerView *picker;
+    NSArray *ballParkArray;
+    IBOutlet UITextField *tf1;
+    IBOutlet UITextField *tf2;
+    IBOutlet UITextField *tf3;
+    IBOutlet UITextField *tf4;
+    IBOutlet UILabel *label1;
+    IBOutlet UILabel *label2;
+    IBOutlet UILabel *label3;
+    IBOutlet UILabel *label4;
+
+
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    picker.delegate = self;
+    
+    // データソースを設定
+    picker.dataSource = self;
+    
+    // 選択インジケータを表示
+    picker.showsSelectionIndicator = YES;
+    
+    // UIPickerのインスタンスをビューに追加
+    ballParkArray = [NSArray arrayWithObjects:@"横浜スタジアム",@"東京ドーム",@"神宮球場",nil];
+    label1.text=(@"塁側");
+     label2.text=(@"通路");
+     label3.text=(@"列");
+     label4.text=(@"番");
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+
+//ピッカーに表示する行数を返す
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView
+numberOfRowsInComponent:(NSInteger)component
+{
+    return ballParkArray.count;
+}
+
+
+// 行のサイズを変更
+
+
+//ピッカーに表示する値を返す
+
+- (NSString *)pickerView:(UIPickerView *)pickerView
+             titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return ballParkArray[row];}
+
+
+//ピッカーの選択行が決まったとき
+
+- (void)pickerView:(UIPickerView *)pickerView
+      didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    NSLog(@"%@が選択されました",ballParkArray[row]);
+}
+
 
 @end
